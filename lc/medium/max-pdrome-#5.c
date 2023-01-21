a080ce0b47732b97b9577f6a1a4e5c12e27077f3
Rahul Sharma <rsharma842756@gmail.com>
2023-01-21 12:38:44 +0530

    Correct lc #5 (medium)

--- lc/medium/max-pdrome-#5.c
+++ lc/medium/max-pdrome-#5.c
@@ -4,14 +4,14 @@
 #include <string.h>
 
 char * max_pdrome(char *str);
-int grow_pdrome(char *str, int len, int *ptr1, int *ptr2);
+int grow_pdrome(char *str, int ptr1, int ptr2);
 
 int main(void) {
     /* not passing input string 'literally' to
      * max_pdrome as it trunicates the overwriting
      * a character with null which produces segfault
      * as the 'literal' string is read-only */
-    char input[] = "dgtyracecargihj";
+    char input[] = "dsbba";
     char *pdrome = max_pdrome(input);
 
     printf("Output: %s\n", pdrome);
@@ -19,45 +19,34 @@ int main(void) {
 }
 
 char * max_pdrome(char *str) {
-    int beg, end, slen, p1, p2, i, curlen, pdrome_len;
+    int beg, end, slen, len1, len2, len;
 
     beg = end = 0;
-    pdrome_len = 1;
     slen = strlen(str);
 
-    for (i = 1; i < slen-1; ++i) {
+    for (int i = 0; i < slen; ++i) {
+        len1 = grow_pdrome(str, i, i);
+        len2 = grow_pdrome(str, i, i+1);
 
-        p1 = p2 = i;
-        if ((curlen = grow_pdrome(str, slen, &p1, &p2)) > pdrome_len) {
-            beg = p1;
-            end = p2;
-            pdrome_len = curlen;
+        if (len1 > len2)
+            len = len1;
+        else
+            len = len2;
+
+        if (len > end - beg) {
+            beg = i - (len - 1) / 2;
+            end = i + len / 2;
         }
-
-        p1 = i;
-        p2 = i + 1;
-        if ((curlen = grow_pdrome(str, slen, &p1, &p2)) > pdrome_len) {
-            beg = p1;
-            end = p2;
-            pdrome_len = curlen;
-        }
-
-        if (pdrome_len == slen)
-            break;
     }
-
     str[end+1] = '\0';
     return &str[beg];
 }
 
-int grow_pdrome(char *str, int len, int *ptr1, int *ptr2) {
-    while (*ptr1 > 0 && *ptr2 < len) {
-        if (str[*ptr1 - 1] == str[*ptr2 + 1]) {
-            *ptr1 = *ptr1 - 1;
-            *ptr2 = *ptr2 + 1;
-        }
-        else
-            break;
+int grow_pdrome(char *str, int ptr1, int ptr2) {
+    int len = strlen(str);
+    while (ptr1 >= 0 && ptr2 < len && (str[ptr1] == str[ptr2])) {
+        ptr1--;
+        ptr2++;
     }
-    return *ptr2 - *ptr1 + 1;
+    return ptr2 - ptr1 - 1;
 }
