41346c65dacc75c410a71c8e0217f6303dd6386a
Rahul Sharma <rsharma842756@gmail.com>
2023-01-21 11:40:03 +0530

    Corrected 3sum (#16)

--- lc/easy/3sum-#16.c
+++ lc/easy/3sum-#16.c
@@ -9,27 +9,38 @@ unsigned int abs_val(int val);
 int cmpfunc (const void * a, const void * b);
 
 int main(void) {
-    int result, nums[] = {5, 12, -9, 43, 2, -65, 3, 4, 19, 23, -11};
+    // int result, nums[] = {5, 12, -9, 43, 2, -65, 3, 4, 19, 23, -11};
+    int result, nums[] = {2, 100, 150, 8, 10, 200};
 
-    result = closest_triplet_sum(nums, sizeof(nums)/sizeof(nums[0]), 40);
+    result = closest_triplet_sum(nums, sizeof(nums)/sizeof(nums[0]), 214);
     printf("Closest sum: %d\n", result);
     return 0;
 }
 
 int closest_triplet_sum(int* arr, int len, int target) {
-    qsort(arr, len, sizeof(int), cmpfunc);
-
-    int i, closest, found_sum;
+    int closest, sum, p1, p2, two_sum;
     closest = arr[0] + arr[1] + arr[2];
 
-    for (i = 0; i < len - 2; ++i) {
-        found_sum = closest_duet_sum(arr + (i+1), len - (i+1), target - arr[i]) + arr[i];
+    qsort(arr, len, sizeof(int), cmpfunc);
 
-        if (abs_val(closest - target) > abs_val(found_sum - target))
-            closest = found_sum;
+    for (int i = 0; i < len - 2; ++i) {
+        p1 = i + 1;
+        p2 = len - 1;
 
-        if (closest == target)
-            break;
+        while (p1 < p2) {
+            two_sum = arr[p1] + arr[p2];
+            sum = two_sum + arr[i];
+
+            if (two_sum == target - arr[i])
+                return sum;
+            else if (two_sum > target - arr[i])
+                p2--;
+            else
+                p1++;
+
+            if (abs_val(target-sum) < abs_val(target-closest))
+                closest = sum;
+        }
     }
     return closest;
 }
@@ -39,20 +50,6 @@ unsigned int abs_val(int val) {
         return val * -1;
     else
         return val;
-}
-
-int closest_duet_sum(int* arr, int len, int target) {
-    int p1, p2, sum;
-    p1 = 0;
-    p2 = len - 1;
-
-    while ((p1 != p2) && ((sum = arr[p1] + arr[p2]) != target)) {
-        if (sum < target)
-            p1 += 1;
-        else if (sum > target)
-            p2 -= 1;
-    }
-    return sum;
 }
 
 int cmpfunc (const void * a, const void * b) {
