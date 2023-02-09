57a6435f0080b213c71100581ee3a0f161633d48
Rahul Sharma <rsharma842756@gmail.com>
2023-02-09 21:32:25 +0530

    Add lc 26 (easy, array, two pointer)

---
+++ lc/easy/rm-dup-sorted-#26.c
@@ -0,0 +1,44 @@
+/* Remove all multiple occurrences for each element in a sorted array. */
+
+#include <stdio.h>
+
+void print_arr(const int *arr, const int size);
+int removeDuplicates(int* nums, int numsSize);
+int main(void) {
+    int size, nunique;
+    int nums[] = {2, 2, 3, 3, 4, 5, 6 , 7, 7};
+    size = sizeof(nums)/sizeof(nums[0]);
+
+    /* Print Input. */
+    printf("I: [");
+    print_arr(nums, size);
+    printf("]\n");
+
+    nunique = removeDuplicates(nums, size);
+    /* Print Output. */
+    printf("O: [");
+    print_arr(nums, size);
+    printf("], %d\n", nunique);
+    return 0;
+}
+
+void print_arr(const int *arr, const int size) {
+    for (int i = 0; i < size; ++i) {
+        printf("%d", arr[i]);
+        if (i != size-1) {
+            printf(", ");
+        }
+    }
+}
+
+int removeDuplicates(int* nums, int numsSize){
+    int i, j;
+    i = 0;
+    for (j = 1; j < numsSize; j++) {
+        if (nums[j] != nums[j-1]) {
+            i++;
+            nums[i] = nums[j];
+        }
+    }
+    return i+1;
+}
