bf83097d06d7a1f033c73e322a129b5ee1faa94b
Rahul Sharma <rsharma842756@gmail.com>
2023-02-10 06:27:27 +0530

    Add lc 704 (easy, bsearch)

---
+++ lc/easy/binary-search-#792.c
@@ -0,0 +1,48 @@
+/* plain old binary search */
+
+#include <stdio.h>
+
+int search(int* nums, int numsSize, int target);
+void print_arr(const int *arr, const int size);
+int main(void) {
+    int size, target;
+    int nums[] = {3, 5, 8, 11, 12, 15, 17, 18, 21};
+    size = sizeof(nums)/sizeof(nums[0]);
+    target = 11;
+
+    /* Print Input. */
+    printf("I: [");
+    print_arr(nums, size);
+    printf("], %d\n", target);
+
+    /* Print Output. */
+    printf("O: %d\n", search(nums, size, target));
+    return 0;
+}
+
+int search(int* nums, int numsSize, int target){
+    int l, r, m;
+    l = 0;
+    r = numsSize - 1;
+
+    while (l <= r) {
+        m = (l+r) / 2;
+        if (target < nums[m])
+            r = m - 1;
+        else if (target > nums[m])
+            l = m + 1;
+        else
+            return m;
+    }
+    return -1;
+}
+
+void print_arr(const int *arr, const int size) {
+    for (int i = 0; i < size; ++i) {
+        printf("%d", arr[i]);
+        if (i != size-1) {
+            printf(", ");
+        }
+    }
+}
+
